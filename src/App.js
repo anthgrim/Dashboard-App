0ff64edf49c924915e6c92d0ea5d41c017a5cf66
Kevin Grimaldi <anthgrim@icloud.com>
2022-07-02 21:38:59 -0400

    Initial set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <h1 className="underline text-3xl">App</h1>
+    </>
+  );
 }
 
 export default App;
